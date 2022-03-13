cf4000c967dc4f671c298411b22c5eaf16478456
Abbe98 <albin.post@gmail.com>
2022-03-13 12:01:10 +0100

    make styles more consistent across sites

--- extension/src/content_script.js
+++ extension/src/content_script.js
@@ -42,7 +42,7 @@ function render(accountInformation) {
                        </div>
                        <div>
                          <button id="govdirectory-browser-extension-namespace-close" title="close">
-                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/><path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/></svg>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000" class="bi bi-x-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/><path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/></svg>
                          </button>
                          <h1>This account appears to belong to a government agency (${accountInformation[0]})</h1>
                          <a href="https://www.govdirectory.org/${accountInformation[1]}">Learn more on Govdirectory</a>
